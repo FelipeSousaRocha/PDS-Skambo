c8dd08c1545faf6b26146c5388c0bcb3f72b82e9
FelipeSousaRocha <felipedsousarocha@gmail.com>
2022-02-14 15:42:09 +0000

    pesquisa 02

--- skambo/views.py
+++ skambo/views.py
@@ -1,13 +1,6 @@
 from django.shortcuts import render, Produto
 
 def index(request):
-    #busca
-    search = request.GET.get('search')
-
-    if search:
-        list = Produto.objects.filter(descricao__icontains=search)
-
+    produto = Produto.objects.filter(descricao__icontains=search)
 
     return render(request,"skambo/index.html")
-
-
