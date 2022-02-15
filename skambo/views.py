d6f15f65b4ca2d5a715bc0ead158028528b14564
FelipeSousaRocha <felipedsousarocha@gmail.com>
2022-02-15 00:13:22 +0000

    att

--- skambo/views.py
+++ skambo/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, Produto
 
 def skambo(request):
+    search = Produto.objects.filter(title__icontains)
     return render(request,"skambo/skambo.html")
