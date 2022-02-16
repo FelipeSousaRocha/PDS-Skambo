50b15bf93cdc09394fe54fc7d4624dbb5bbee70f
FelipeSousaRocha <felipedsousarocha@gmail.com>
2022-02-16 00:12:13 +0000

    Insercao de view e modificacao no skambo.html

--- skambo/views.py
+++ skambo/views.py
@@ -1,17 +1,15 @@
-from django.shortcuts import render
-from django.template import loader
+from django.shortcuts import render, get_object_or_404
 from .models import Anuncio
-from django.http import HttpResponse
 
 def skambo(request):
     #search = Produto.objects.filter(title__icontains)
-    anuncios = Anuncio.objects.all()
-    template = loader.get_template('skambo/skambo.html')
+    anuncios = Anuncio.objects.order_by('-data')[:10]
     contexto = {
         'anuncios': anuncios,
         }
-    return HttpResponse(template.render(contexto, request))
+    return render(request, 'skambo/skambo.html', contexto)
 
-def anuncio(request):
-    return render(request,"skambo/anuncio.html")
+def anuncio(request, id_anuncio):
+    anuncio = get_object_or_404(Anuncio, pk = id_anuncio)
+    return render(request, 'skambo/anuncio.html', {'anuncio': anuncio})
 
