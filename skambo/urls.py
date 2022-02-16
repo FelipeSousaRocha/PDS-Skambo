3f09433aa1b3d9da36d4aacf9d9df023cd133b94
FelipeSousaRocha <felipedsousarocha@gmail.com>
2022-02-16 22:24:11 +0000

    Adicionando template CSS

--- skambo/urls.py
+++ skambo/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'skambo'
 urlpatterns = [
-    path('', views.skambo, name="skambo"),
-    path('', views.anuncio, name="anuncio"),
+    path('', views.skambo, name="index"),
+    path('anuncio/<int:pk>/', views.anuncio, name="anuncio"),
+    path('products/', views.products, name="products"),
+    path('singleproduct/', views.singleproduct, name="singleproduct"),
+    path('contact/', views.contact, name="contact"),
+    path('about/', views.about, name="about"),
     #django-allauth
-    ]
+]
--- skambo/views.py
+++ skambo/views.py
@@ -7,9 +7,20 @@ def skambo(request):
     contexto = {
         'anuncios': anuncios,
         }
-    return render(request, 'skambo/skambo.html', contexto)
+    return render(request, 'skambo/index.html', contexto)
 
-def anuncio(request, id_anuncio):
-    anuncio = get_object_or_404(Anuncio, pk = id_anuncio)
+def anuncio(request, pk):
+    anuncio = get_object_or_404(Anuncio, pk = pk)
     return render(request, 'skambo/anuncio.html', {'anuncio': anuncio})
 
+def products(request):
+    return render(request, 'skambo/products.html')
+
+def about(request):
+    return render(request, 'skambo/about.html')
+
+def contact(request):
+    return render(request, 'skambo/contact.html')
+
+def singleproduct(request):
+    return render(request, 'skambo/singleproduct.html')
